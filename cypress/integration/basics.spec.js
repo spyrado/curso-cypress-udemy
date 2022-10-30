5b4fba53075b485dee1fc8587dcf22ff082a1d4a
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2022-10-30 01:10:44 -0300

    att conhecimento

--- cypress/integration/basics.spec.js
+++ cypress/integration/basics.spec.js
@@ -4,6 +4,8 @@ describe('Cypress basics', () => {
   
   it('should visit a page and assert a title', () => {
     cy.visit('https://wcaquino.me/cypress/componentes.html');
-    cy.title().should('be.equal', 'Campo de Treinamento');
+    cy.title()
+      .should('be.equal', 'Campo de Treinamento')
+      .and('contains', 'Campo');
   });
 });
